8bdcfb20b5cc7d6e67db1aaf8366fa89bf55b68b
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-07 13:25:14 +0100

    Slug function was updated

--- prisma/migrations/20260605555555-slug-func/migration.sql
+++ prisma/migrations/20260605555555-slug-func/migration.sql
@@ -1,10 +1,18 @@
 CREATE OR REPLACE FUNCTION create_slug(text_input TEXT)
     RETURNS TEXT AS $$
 BEGIN
-    RETURN LOWER(
-        REGEXP_REPLACE(
-                REGEXP_REPLACE(text_input, '[^a-zA-Z0-9\s-]', '', 'g'),'\s+', '-', 'g'
-        )
-       );
+    RETURN trim(both '-' from
+                regexp_replace(
+                        regexp_replace(
+                                lower(text_input),
+                                '[^a-zа-яіїєґ0-9\s-]',
+                                '',
+                                'gi'
+                        ),
+                        '\s+',
+                        '-',
+                        'g'
+                )
+           );
 END;
 $$ LANGUAGE plpgsql IMMUTABLE;
