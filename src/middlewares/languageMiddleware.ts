cd67bccd6373048ce1082562bb90cec1c2b39354
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-08 11:40:01 +0100

    Language middleware was added

---
+++ src/middlewares/languageMiddleware.ts
@@ -0,0 +1,20 @@
+import { NextFunction, Request, Response } from 'express'
+
+import { Language } from '../utils/enums';
+
+export default function languageMiddleware(
+    req: Request,
+    _res: Response,
+    next: NextFunction
+) {
+    const acceptLanguage = req.headers['accept-language'];
+
+    if (
+        !acceptLanguage
+        || !Object.values(Language).includes(acceptLanguage as Language)
+    ) {
+        req.headers['accept-language'] = Language.en;
+    }
+
+    next();
+}
