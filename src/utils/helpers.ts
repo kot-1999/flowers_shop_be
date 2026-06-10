0c5dd3afc572437c3bbf00cc45b85924a710f074
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-10 20:49:30 +0100

    slug functions were updated

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,10 +2,14 @@ import { Language } from './enums';
 
 export const translationSelect = Object.fromEntries(Object.values(Language).map((lang) => [lang, true]));
 
-export function slugify(textInput: string): string {
-    return textInput
+export function slugify(text: string): string {
+    return text
+        .normalize('NFD') // split letters + accents
+        .replace(/[\u0300-\u036f]/g, '') // remove accents
         .toLowerCase()
-        .replace(/[^a-zа-яіїєґ0-9\s-]/giu, '')
+        .replace(/ß/g, 'ss')
+        .replace(/[^a-z0-9\s-]/g, '')
+        .trim()
         .replace(/\s+/g, '-')
-        .replace(/^-+|-+$/g, '')
+        .replace(/-+/g, '-')
 }
