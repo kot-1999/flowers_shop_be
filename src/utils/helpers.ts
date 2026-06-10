f9462599724cfec74bf8e356decd307205909933
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-10 14:23:18 +0100

    slugify function was added

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,11 @@
 import { Language } from './enums';
 
 export const translationSelect = Object.fromEntries(Object.values(Language).map((lang) => [lang, true]));
+
+export function slugify(textInput: string): string {
+    return textInput
+        .toLowerCase()
+        .replace(/[^a-zа-яіїєґ0-9\s-]/giu, '')
+        .replace(/\s+/g, '-')
+        .replace(/^-+|-+$/g, '')
+}
