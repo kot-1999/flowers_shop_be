ac20da0404fd21fd845774bb4e0be6a6546ef728
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-20 01:12:15 +0100

    user controller was updated with get users and patch users

--- src/routes/v1/UserRouter.ts
+++ src/routes/v1/UserRouter.ts
@@ -1,7 +1,9 @@
+import { UserRole } from '@prisma/client'
 import { Router } from 'express'
 
 import { UsersController } from '../../controllers/v1/UserController'
 import authorizationMiddleware from '../../middlewares/authorizationMiddleware'
+import permissionMiddleware from '../../middlewares/permissionMiddleware'
 import validationMiddleware from '../../middlewares/validationMiddleware'
 import { PassportStrategy } from '../../utils/enums'
 
@@ -27,6 +29,44 @@ export default function authorizationRouter() {
         validationMiddleware(UsersController.schemas.request.getUser),
         authorizationMiddleware([PassportStrategy.google]),
         userController.getUser
+    )
+
+    router.get(
+        /*
+            #swagger.tags = ['v1-User']
+            #swagger.description = 'Get users list',
+            #swagger.parameters['body'] = {
+                in: 'body',
+                schema: { $ref: '#/definitions/v1GetUsersReqBody' }
+            }
+            #swagger.responses[200] = {
+                schema: { "$ref": "#/definitions/v1GetUsersRes" },
+            }
+        */
+        '/',
+        validationMiddleware(UsersController.schemas.request.getUsers),
+        authorizationMiddleware([PassportStrategy.google]),
+        permissionMiddleware([UserRole.Admin]),
+        // @ts-ignore
+        userController.getUsers
+    )
+
+    router.patch(
+        /*
+            #swagger.tags = ['v1-User']
+            #swagger.description = 'Get users list',
+            #swagger.parameters['body'] = {
+                in: 'body',
+                schema: { $ref: '#/definitions/v1PatchUserReqBody' }
+            }
+            #swagger.responses[200] = {
+                schema: { "$ref": "#/definitions/v1PatchUserRes" },
+            }
+        */
+        '/:userID',
+        validationMiddleware(UsersController.schemas.request.patchUser),
+        authorizationMiddleware([PassportStrategy.google]),
+        userController.patchUser
     )
 
     router.delete(
