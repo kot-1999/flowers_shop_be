28b69d331f87192d7433212b9be02be16b87f473
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-06-24 18:07:50 +0100

    Controller was implemented

--- src/controllers/v1/BasketController.ts
+++ src/controllers/v1/BasketController.ts
@@ -1,8 +1,10 @@
 import { AuthRequest, NextFunction, Response } from 'express'
 import Joi from 'joi'
 
+import prisma from '../../services/Prisma'
 import { AbstractController } from '../../types/AbstractController'
 import { JoiCommon } from '../../types/JoiCommon'
+import { IError } from '../../utils/IError'
 
 export class BasketController extends AbstractController {
     private static readonly basketItemSchema = Joi.object({
@@ -93,14 +95,76 @@ export class BasketController extends AbstractController {
 
     private GetBasketItemsReqType: Joi.extractType<typeof BasketController.schemas.request.getBasketItems>
     private GetBasketItemsResType: Joi.extractType<typeof BasketController.schemas.response.getBasketItems>
-
     public async getBasketItems(
         req: AuthRequest & typeof this.GetBasketItemsReqType,
         res: Response<typeof this.GetBasketItemsResType>,
         next: NextFunction
     ) {
         try {
-            //
+            const { user } = req
+            const language = req.headers['accept-language']
+            const basketItems = await prisma.basketItem.findMany({
+                where: {
+                    userID: user.id
+                },
+                orderBy: {
+                    createdAt: 'desc'
+                },
+                select: {
+                    id: true,
+                    quantity: true,
+                    createdAt: true,
+
+                    pricing: {
+                        select: {
+                            id: true,
+                            price: true,
+                            quantity: true,
+
+                            itemType: {
+                                select: {
+                                    id: true,
+                                    weight: true,
+
+                                    name: {
+                                        select: {
+                                            [language as string]: true
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+
+                    good: {
+                        select: {
+                            id: true,
+                            photos: true,
+
+                            name: {
+                                select: {
+                                    [language as string]: true
+                                }
+                            },
+
+                            category: {
+                                select: {
+                                    id: true,
+                                    name: {
+                                        select: {
+                                            [language as string]: true
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+
+            return res.status(200).json({
+                basketItems
+            })
         } catch (err) {
             return next(err)
         }
@@ -114,7 +178,70 @@ export class BasketController extends AbstractController {
         next: NextFunction
     ) {
         try {
-            //
+            const { body, user } = req
+
+            const pricing = await prisma.pricing.findFirst({
+                where: {
+                    id: body.pricingID,
+                    deletedAt: null
+                },
+                select: {
+                    id: true,
+                    quantity: true
+                }
+            })
+
+            if (!pricing) {
+                throw new IError(404, req.t('Pricing not found'))
+            }
+
+            const quantity = Math.min(
+                body.quantity,
+                pricing.quantity
+            )
+
+            const existingItem = await prisma.basketItem.findUnique({
+                where: {
+                    goodID_userID_pricingID: {
+                        goodID: body.goodID,
+                        pricingID: body.pricingID,
+                        userID: user.id
+                    }
+                }
+            })
+
+            let basketItem
+
+            if (existingItem) {
+                basketItem = await prisma.basketItem.update({
+                    where: {
+                        id: existingItem.id
+                    },
+                    data: {
+                        quantity: Math.min(existingItem.quantity + quantity, pricing.quantity)
+                    },
+                    select: {
+                        id: true
+                    }
+                })
+            } else {
+                basketItem = await prisma.basketItem.create({
+                    data: {
+                        goodID: body.goodID,
+                        pricingID: body.pricingID,
+                        userID: user.id,
+                        quantity
+                    },
+                    select: {
+                        id: true
+                    }
+                })
+            }
+
+            return res.status(200).json({
+                basketItem,
+                message: req.t('Basket item created')
+            })
         } catch (err) {
             return next(err)
         }
@@ -128,7 +255,56 @@ export class BasketController extends AbstractController {
         next: NextFunction
     ) {
         try {
-            //
+            const { params, body, user } = req
+
+            const basketItem = await prisma.basketItem.findFirst({
+                where: {
+                    id: params.basketItemID,
+                    userID: user.id
+                },
+                select: {
+                    id: true,
+                    pricingID: true
+                }
+            })
+
+            if (!basketItem) {
+                throw new IError(404, req.t('Basket item not found'))
+            }
+
+            const pricing = await prisma.pricing.findFirst({
+                where: {
+                    id: basketItem.pricingID,
+                    deletedAt: null
+                },
+                select: {
+                    quantity: true
+                }
+            })
+
+            if (!pricing) {
+                throw new IError(404, req.t('Pricing not found'))
+            }
+
+            const updated = await prisma.basketItem.update({
+                where: {
+                    id: basketItem.id
+                },
+                data: {
+                    quantity: Math.min(
+                        body.quantity,
+                        pricing.quantity
+                    )
+                },
+                select: {
+                    id: true
+                }
+            })
+
+            return res.status(200).json({
+                basketItem: updated,
+                message: req.t('Basket item updated')
+            })
         } catch (err) {
             return next(err)
         }
@@ -142,7 +318,31 @@ export class BasketController extends AbstractController {
         next: NextFunction
     ) {
         try {
-            //
+            const { params, user } = req
+
+            const basketItem = await prisma.basketItem.findFirst({
+                where: {
+                    id: params.basketItemID,
+                    userID: user.id
+                },
+                select: {
+                    id: true
+                }
+            })
+
+            if (!basketItem) {
+                throw new IError(404, req.t('Basket item not found'))
+            }
+
+            await prisma.basketItem.delete({
+                where: {
+                    id: basketItem.id
+                }
+            })
+
+            return res.status(200).json({
+                message: req.t('Basket item deleted')
+            })
         } catch (err) {
             return next(err)
         }
